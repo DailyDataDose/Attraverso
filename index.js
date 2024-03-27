8b3ad1668f79a45b26895013082ec1815d90afa7
Alex <alex_rmzz95@hotmail.com>
2024-03-27 13:01:58 -0600

    Alpha Version 4

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ navLinks.forEach(link => {
         targetSection.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+//For Fade in animation on text
+$(document).ready(function() {
+    // Function to check if an element is in the viewport
+    function isInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+
+    // Function to handle the scroll event
+    function handleScroll() {
+      $('.fade-in').each(function() {
+        if (isInViewport(this)) {
+          $(this).addClass('fade-in-active');
+        }
+      });
+    }
+
+    // Initial check when the page loads
+    handleScroll();
+
+    // Event listener for scroll event
+    $(window).on('scroll', handleScroll);
+  });
